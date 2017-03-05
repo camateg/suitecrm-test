0129c7578d355a39d9e2d6ca5d1152fdd5747845
Matthew Fleeger <mfleeger5@gmail.com>
2017-03-05 11:35:33 -0500

    document changes.

--- document.php
+++ document.php
@@ -4,19 +4,36 @@
 
     $set_entry_parameters = array(
          "session" => $session_id,
-         "module_name" => "Documents",
+         "module_name" => "Cases",
          "name_value_list" => array(
-              array("name" => "document_name", "value" => $_POST['name']),
+              array("name" => "name", "value" => $_POST['title']),
+              array("name" => "description", "value" => $_POST['body']),
+              array("name" => "account_id", "value" => $_POST['account']),
          ),
     );
 
     $set_entry_result = call("set_entry", $set_entry_parameters, $url);
 
-    $did = $set_entry_result->id;
+    $cid = $set_entry_result->id;
 
-    $cid = $_POST['case_id'];
+    $get_entry_parameters = array(
+        "session" => $session_id,
+        "module_name" => "Cases",
+        "id" => $cid,
+        "select_fields" => array(
+	    "id",
+            "name",
+            "case_number",
+        ),
+        'link_name_to_fields_array' => [],
+    );
 
+    $get_entry_result = call("get_entry", $get_entry_parameters, $url);
     
+    $case_name = $get_entry_result->entry_list[0]->name_value_list->name->value;
+    $case_number = $get_entry_result->entry_list[0]->name_value_list->case_number->value;
+
+    $cases = array('id' => $cid, 'name' => $case_name, 'number' => $case_number);
 
     header('Content-type: application/json');
     echo json_encode($cases);
